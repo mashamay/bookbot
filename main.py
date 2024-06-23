ac71acdcc8b86f725e64a3b0b50035e7eb21dda3
mashamay <mashamay@gmail.com>
2024-06-23 10:09:46 +0200

    add char_dict

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(get_num_words(text))
+    print(get_chars_dict(text))
 
 # count words in a text
 def get_num_words(text):
@@ -12,5 +12,18 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_chars_dict(text):
+    lowered_text = text.lower()
+    chars_dict = {}
+
+    for char in lowered_text:
+        # add char if not in dict
+        if char not in chars_dict:
+            chars_dict[char] = 1
+        # else increase counter
+        else:
+            chars_dict[char] += 1
+    return chars_dict
 
 main()
