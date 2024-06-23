25398f160f68163c9370d1c42b0fda9ed74d8f3c
mashamay <mashamay@gmail.com>
2024-06-23 09:49:03 +0200

    add get_num_words and get_book_text functions

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(get_num_words(text))
+
+# count words in a text
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+# read book text from a file path
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
