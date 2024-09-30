80711d60611aebce905554211e6216ea52464a2d
mashamay <mashamay@gmail.com>
2024-09-30 13:21:50 +0200

    Added print report function

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
+import os, sys
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(get_chars_dict(text))
+
+    # a dictionary with count of chars [{'p': 1234}; {'o' : 987}; ... ]
+    chars_dict = get_chars_dict(text)
+
+    # convert your dictionary of characters into a list of dictionaries
+    #chars_dict chars = get_list_of_dict(chars_dict)
+
+    print_report(text)
 
 # count words in a text
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+    
+def print_report(text):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{get_num_words(text)} words found in the document")
+    
+    chars_dict = get_chars_dict(text)
+
+    for char in sorted(chars_dict, key=chars_dict.get, reverse=True):
+        if char.isalpha():
+            print(f'The "{char}" character was found {chars_dict[char]} times')
+
+    print("--- End report ---")
+
+# convert dictionary of characters into a list of dictionaries
+#def get_list_of_dict(chars_dict):
+ #   chars_list = []
+
+  #  for item in chars_dict: 
+   #     new_dict[item] = chars_dict[item]
+        #print(chars_dict[item])
+    #return new_dict
+
 # read book text from a file path
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_chars_dict(text):
-    lowered_text = text.lower()
-    chars_dict = {}
-
-    for char in lowered_text:
-        # add char if not in dict
-        if char not in chars_dict:
-            chars_dict[char] = 1
-        # else increase counter
-        else:
-            chars_dict[char] += 1
-    return chars_dict
-
+#print ("Current working dir : %s" % os.getcwd())
 main()
